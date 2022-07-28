ce316a609bfa822def028ec7be81ac7857a7f3d0
Karenandreagomez <karenandreag98@hotmail.com>
2022-07-28 14:07:28 -0500

    Add files via upload

---
+++ practica_while.py
@@ -0,0 +1,13 @@
+contador = 1
+print(contador)
+while contador < 1000:
+  contador += 1
+  print(contador)
+  
+
+for contador in range(1, 11):
+    print(contador)
+
+frase = input('Escribe un nombre:')
+for caracter in frase:
+  print(frase)
