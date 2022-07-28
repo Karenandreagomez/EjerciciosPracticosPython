0d848c11ab7e0ce129d1e5c64706d764f1b5a44b
Karenandreagomez <karenandreag98@hotmail.com>
2022-07-28 14:06:45 -0500

    Add files via upload

---
+++ diccionario2.py
@@ -0,0 +1,11 @@
+def run():
+ datos_personales = {
+    'Nombre' : 'Karen',
+    'Apellido' : 'Gomez',
+    'Edad' : 24
+ }
+ for datos in datos_personales.items():
+    print(datos)
+ 
+if __name__ == '__main__':
+    run()
